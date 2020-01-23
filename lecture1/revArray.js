e58e655235bd7959bae2748e3c0a43ad19b43f43
zoyron <sgrarora3@gmail.com>
2020-01-23 13:26:41 +0530

    second commit

--- lecture1/revArray.js
+++ lecture1/revArray.js
@@ -1,2 +1,11 @@
-let arr1 = [1,2,3,4,5];
-console.log(arr1.reverse());
+// let arr1 = [1,2,3,4,5];
+// console.log(arr1.reverse());
+
+let cap = {
+    name : "sagar",
+    lastName : "arora"
+}
+
+for(let key in cap){
+    console.log(key+ " : "+cap[key]);
+}
